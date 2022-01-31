7a68b9003f46b818bc37171019e153173d74a16f
Edoardo Lai <edoardolai96@gmail.com>
2022-01-31 18:57:34 +0100

    esercizio terminato

--- main.js
+++ main.js
@@ -6,39 +6,110 @@ const container = document.getElementById("container");
 
 const difficoltà = document.getElementById("difficoltà");
 
-const easy = document.getElementById("easy");
+difficoltà.value = "";
 
+const play = document.getElementById("play");
 
+const reset = document.getElementById("reset");
 
-if(difficoltà.value  == easy){
+play.addEventListener("click", function(){
 
     
-    for (i=1; i <= 100; i++){
+    
+    
+    if(difficoltà.value == "easy"){
         
-        // creo dallo script stesso gli element che voglio aggiungere 
-        const node = document.createElement("div");
-        node.classList.add("square");
+        container.innerHTML= "";
         
-        const nodeText = document.createElement("p");
-        node.appendChild(nodeText);
-        nodeText.innerHTML = i;
-        // aggiungo a ogni elemento che è stato creato la classe già pronta del mio css 
-        
-        
-        // Aggiungo all'interno del container gli elementi creati e stilizzati (con il metodo append) 
-        container.appendChild(node);
-        
-        node.addEventListener("click", function(){
-            this.classList.add("clicked");
+        for (i=1; i <= 100; i++){
+            
+            // creo dallo script stesso gli element che voglio aggiungere 
+            const node = document.createElement("div");
+            node.classList.add("square-10");
+            
+            const nodeText = document.createElement("p");
+            node.appendChild(nodeText);
+            nodeText.innerHTML = i;
+            // aggiungo a ogni elemento che è stato creato la classe già pronta del mio css 
+            
+            
+            // Aggiungo all'interno del container gli elementi creati e stilizzati (con il metodo append) 
+            container.appendChild(node);
+            
+            node.addEventListener("click", function(){
+                this.classList.add("clicked");
+                
+            }
+            
+            )
+            
             
         }
         
-        )
+    } else if (difficoltà.value == "hard"){
         
+        container.innerHTML= "";
+        for (i=1; i <= 81; i++){
+            
+            // creo dallo script stesso gli element che voglio aggiungere 
+            const node = document.createElement("div");
+            node.classList.add("square-9");
+            
+            const nodeText = document.createElement("p");
+            node.appendChild(nodeText);
+            nodeText.innerHTML = i;
+            // aggiungo a ogni elemento che è stato creato la classe già pronta del mio css 
+            
+            
+            // Aggiungo all'interno del container gli elementi creati e stilizzati (con il metodo append) 
+            container.appendChild(node);
+            
+            node.addEventListener("click", function(){
+                this.classList.add("clicked");
+                
+            }
+            
+            )
+            
+            
+            
+        }
         
+    } else if (difficoltà.value == "crazy"){
+        
+        container.innerHTML= "";
+        for (i=1; i <= 49; i++){
+            
+            // creo dallo script stesso gli element che voglio aggiungere 
+            const node = document.createElement("div");
+            node.classList.add("square-7");
+            
+            const nodeText = document.createElement("p");
+            node.appendChild(nodeText);
+            nodeText.innerHTML = i;
+            // aggiungo a ogni elemento che è stato creato la classe già pronta del mio css 
+            
+            
+            // Aggiungo all'interno del container gli elementi creati e stilizzati (con il metodo append) 
+            container.appendChild(node);
+            
+            node.addEventListener("click", function(){
+                this.classList.add("clicked");
+                
+            }
+            
+            )
+            
+            
+            
+        }
         
     }
     
-}
+})
 
+
+reset.addEventListener("click", function () {
+    container.innerHTML= "";
+})
 
